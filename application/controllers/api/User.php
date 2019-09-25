7d946e4fa9722ad8bef9cce8532657230f529fd4
shirokun <khoirul.20998@gmail.com>
2019-09-25 14:13:00 +0700

    Fx

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -13,6 +13,7 @@ class User extends REST_Controller
     {
         parent::__construct();
         header("Access-Control-Allow-Origin: *");
+        $this->load->library('Libkirim_email');
     }
 
     private function _cek_user($where = null)
@@ -74,6 +75,19 @@ class User extends REST_Controller
         }
     }
 
+    private function template_daftar($full_name)
+    {
+        $subject = 'Verifikasi Akun';
+
+        $message = 'Terimakasih kepada ' . $full_name;
+        $message .= ' telah mendaftar di aplikasi KlikWow';
+
+        return array(
+            'subject' => $subject,
+            'message' => $message,
+        );
+    }
+
     public function daftar_post($user_role_id = 2)
     {
         $hasilkan               = $this->validasi();
@@ -84,7 +98,12 @@ class User extends REST_Controller
         $data['user_role_id']   = $user_role_id;
         $data['register_date']  = date('Y-m-d H:i:s');
         $data['user_status_id'] = 2;
-
+        $te                     = $this->template_daftar($data['full_name']);
+        $this->libkirim_email->kirim(array(
+            'email'   => $data['email'],
+            'subject' => $te['subject'],
+            'message' => $te['message'],
+        ));
         $q                = $this->Mo_sb->menambah('user', $data);
         $this->arr_result = array(
             'prilude' => array(
@@ -157,8 +176,8 @@ class User extends REST_Controller
                 'status' => $data['status'],
                 'pesan'  => $data['pesan'],
                 'detail' => array(
-                    'userID'    => md5($hasilnya->user_id),
-                    'full_name' => $hasilnya->full_name,
+                    'userID'              => md5($hasilnya->user_id),
+                    'full_name'           => $hasilnya->full_name,
                     'verification_number' => $hasilnya->verification_number,
                 ),
             ),
