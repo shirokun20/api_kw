1fdb5fa3d5f8a2465e72872bf2395d1bff7829a1
shirokun <khoirul.20998@gmail.com>
2019-10-01 11:13:18 +0700

    ixxx

---
+++ application/controllers/api/Layanan.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Layanan extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Load Dependencies
+
+    }
+
+    // List all your items
+    public function index($offset = 0)
+    {
+
+    }
+
+    // Add a new item
+    public function add()
+    {
+
+    }
+
+    //Update one item
+    public function update($id = null)
+    {
+
+    }
+
+    //Delete one item
+    public function delete($id = null)
+    {
+
+    }
+}
+
+/* End of file Layanan.php */
+/* Location: ./application/controllers/api/Layanan.php */
